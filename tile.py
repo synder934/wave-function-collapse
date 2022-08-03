bd5014d4117d9ca5afb0bbb12f298ede6d5141a2
synder <110554156+synder934@users.noreply.github.com>
2022-08-04 00:07:08 +0100

    Update tile.py
    removed unused code

--- tile.py
+++ tile.py
@@ -2,8 +2,6 @@
 from _main import *
 import pygame as pg
 
-
-
 class Tile(pg.sprite.Sprite):
     def __init__(self, fileName: str, sides: list[int, int, int, int], rotation: int = 0) -> None:
         super().__init__()
@@ -14,12 +12,10 @@ class Tile(pg.sprite.Sprite):
         self.options = []
 
 
-
 class Grid():
     def __init__(self, tiles) -> None:
         self.tiles = tiles
         self.grid = [Cell((x,y), tiles) for x in range(0, DIM[0], TILESIZE[0]) for y in range(0, DIM[1], TILESIZE[1])]
-
 
 
 class Cell(pg.sprite.Sprite):
@@ -42,10 +38,6 @@ class Cell(pg.sprite.Sprite):
         self.adj = []
         pass
 
-    def clicked(self, tile, grid, tiles):
-        print(self.options)
-        pass
-
 
     def tileSet(self, tile:Tile):
         self.tile = tile
@@ -55,36 +47,29 @@ class Cell(pg.sprite.Sprite):
         self.sides = tile.sides.copy()
 
 
-
     def calcOptions(self, grid:Grid, tiles):
         if self.tile != None:
             self.options = [self.tile]
             return
 
-
         self.options = [t for t in tiles]
-
         self.adj = [c for c in grid.grid if c.pos in self.adjCords] if self.adj == [] else self.adj
 
         for cell in self.adj:
-
             if cell.pos == self.adjCords[0]:
                 if cell.sides[2] == 0: pass
                 else:
                     self.options = [tile for tile in self.options if tile.sides[0] == cell.sides[2][::-1]]
-
 
             elif cell.pos == self.adjCords[1]:
                 if cell.sides[3] == 0: pass
                 else:
                     self.options = [tile for tile in self.options if tile.sides[1] == cell.sides[3][::-1]]
 
-
             elif cell.pos == self.adjCords[2]:
                 if cell.sides[0] == 0: pass
                 else:
                     self.options = [tile for tile in self.options if tile.sides[2] == cell.sides[0][::-1]]
-
 
             elif cell.pos == self.adjCords[3]:
                 if cell.sides[1] == 0: pass
@@ -93,19 +78,4 @@ class Cell(pg.sprite.Sprite):
 
         if len(self.options) == 1:
             self.tileSet(self.options[0])
-
-
-    def update(self, grid, tiles) -> None:
-        self.calcOptions(grid, tiles)
-
-        pass
-        
-
-
-
-
-
-
-    
-
-
+            
